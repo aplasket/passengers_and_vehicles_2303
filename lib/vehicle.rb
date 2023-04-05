686ef819f4784d64cbe429c72adc7823deda7cf3
Ashley Plasket <ashley.plasket@gmail.com>
2023-04-05 11:30:16 -0400

    add speeding instance variable, speeding? method and speed method

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,10 +1,20 @@
 class Vehicle
   attr_reader :year,
               :name,
-              :make
+              :make, 
+              :speeding
   def initialize(year, name, make)
     @name = name
     @year = year
     @make = make
+    @speeding = false
+  end
+
+  def speeding?
+    @speeding
+  end
+
+  def speed
+    @speeding = true
   end
 end
--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -14,6 +14,21 @@ RSpec.describe Vehicle do
       expect(@vehicle.year).to eq("2001")
       expect(@vehicle.name).to eq("Honda")
       expect(@vehicle.make).to eq("Civic")
+      expect(@vehicle.speeding).to eq(false)
+    end
+  end
+
+  describe "#speeding?" do
+    it 'checks if vehicles is speeding' do
+      expect(@vehicle.speeding?).to eq(false)
+    end
+  end
+
+  describe '#speed' do
+    it 'can speed' do
+      @vehicle.speed
+      
+      expect(@vehicle.speeding?).to eq(true)
     end
   end
 end
