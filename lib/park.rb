44d7f6c4fb5ce323c2ac77ac69a9dbd971aa8745
Ashley Plasket <ashley.plasket@gmail.com>
2023-04-05 12:06:34 -0400

    add vehicles and instance variable

--- lib/park.rb
+++ lib/park.rb
@@ -1,9 +1,11 @@
 class Park
-  attr_reader :unique_name,
-              :admission_price
+  attr_reader :name,
+              :admission_price,
+              :vehicles
 
   def initialize(park_info)
-    @unique_name = park_info['unique_name']
-    @admission_price = park_info['admission_price']
+    @name = park_info[:name]
+    @admission_price = park_info[:admission_price]
+    @vehicles = []
   end
 end
--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 RSpec.describe Park do
   before(:each) do
-    @forest_park = Park.new({'unique_name' => 'forest park', 'admission_price' => 20})
+    @forest_park = Park.new({:name => 'forest park', :admission_price => 20})
   end
 
   describe '#initialize' do
@@ -11,8 +11,23 @@ RSpec.describe Park do
     end
 
     it 'has attributes' do
-      expect(@forest_park.unique_name).to eq('forest park')
+      expect(@forest_park.name).to eq('forest park')
       expect(@forest_park.admission_price).to eq(20)
+      expect(@forest_park.vehicles).to eq([])
     end
   end
+
+  before(:each) do
+    @vehicle_1 = Vehicle.new("2001", "Honda", "Civic") 
+    @vehicle_2 = Vehicle.new("2008", "Toyota", "Prius") 
+  end
+  
+  describe '#vehicle array' do
+    it 'can add vehicle objects and return an array' do
+      @park.add_vehicle(@vehicle_1)
+      @park.add_vehicle(@vehicle_2)
+      expect(@forest_park.vehicles).to eq([@vehicle_1, @vehicle_2])
+    end
+  end
+
 end
